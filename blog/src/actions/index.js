3f96fa3993e1bdfcb4a715f808ef74406b57629f
JesusMG <jnekok@gmail.com>
2018-01-21 19:17:44 +0100

    removed specific code

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,8 +5,8 @@ export const FETCH_POST = "fetch_post";
 export const CREATE_POST = "create_post";
 export const DELETE_POST = "delete_post";
 
-const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = "?key=PAPERCLIP1234";
+const ROOT_URL = ".....";  // insert url here  like "http://projectname.herokuapp.com/api"
+const API_KEY = ".....";  // insert api key here
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
